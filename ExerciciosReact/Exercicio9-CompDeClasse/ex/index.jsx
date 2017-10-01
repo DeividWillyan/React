95a1637efd780a30affe71f79ab49f4d994e0917
DeividWIllyan <deiviid_@hotmail.com>
2017-10-01 15:56:24 -0300

    Remove duplicated directory

--- ExerciciosReact/Exercicio9-CompDeClasse/ex/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import ClassComponent from './classComponent'
-
-ReactDOM.render(
-    <ClassComponent value="Componente de Classe" />
-    , document.getElementById('app')
-)
